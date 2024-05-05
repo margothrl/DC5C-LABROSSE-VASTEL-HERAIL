aee61acd8c80f965e77c8b3eb756782554de25b6
naissianelab <naisianelabrosse@gmail.com>
2024-05-05 20:25:49 +0200

    Bar chart hypothese 5

---
+++ hypothese5.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./Data_Salary_Market.csv', sep=';')
+
+# Calculer la moyenne des salaires par pays
+salaires_moyens_par_pays = df.groupby('Race')['Salary'].mean().reset_index()
+
+# Afficher un bar chart de la moyenne des salaires en fonction du pays
+plt.figure(figsize=(10, 6))
+bars = plt.bar(salaires_moyens_par_pays['Race'], salaires_moyens_par_pays['Salary'], color='skyblue')
+plt.xlabel('Race')
+plt.ylabel('Moyenne des Salaires')
+plt.title('Moyenne des Salaires par ethnie')
+plt.xticks(rotation=45)
+
+# Ajouter les étiquettes au-dessus de chaque barre
+plt.bar_label(bars, labels=salaires_moyens_par_pays['Salary'].round(2), padding=3)
+plt.tight_layout()  
+plt.show()
